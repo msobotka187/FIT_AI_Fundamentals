e68e95d5da165420db30aec21b2a8d912453a3de
Marek Sobotka <sobotma8@fit.cvut.cz>
2026-03-28 11:04:24 +0100

    Přidána kostra pro TSPGraph

---
+++ 02/TSPGraph.cpp
@@ -0,0 +1,63 @@
+#include "TSPGraph.h"
+#include <random>
+
+TSPGraph::TSPGraph(int numCities, double width, double height) : m_numCities(numCities) {
+  // C++ random generator
+  std::random_device rd;
+  std::mt19937 gen(rd());
+
+  // "Real" numbers form 0 - width/height
+  std::uniform_real_distribution<> disX(0.0, width);
+  std::uniform_real_distribution<> disY(0.0, height);
+
+  // Pass them to vector
+  m_cities.reserve(numCities);
+  for (int i = 0; i < numCities; i++) {
+    m_cities.emplace_back(i, disX(gen), disY(gen));
+  }
+
+  // And calculate distances
+  precomputeDistanceMatrix();
+}
+
+void TSPGraph::precomputeDistanceMatrix() {
+  int n = m_cities.size();
+  // Create empty matrix with zeros
+  m_distanceMatrix.assign(n * n, 0.0);
+
+  // Iterate through upper triangular matrix
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      double dist = m_cities[i].distanceTo(m_cities[j]);
+
+      // Symmetrical write into matrix
+      (*this)[i][j] = dist;
+      (*this)[j][i] = dist;
+    }
+  }
+}
+
+// O(1) lookup
+double TSPGraph::getDistance(int from, int to) const {
+  return (*this)[from][to];
+}
+
+int TSPGraph::getNumCities() const {
+  return m_cities.size();
+}
+
+const City & TSPGraph::getCity(int index) const {
+  return m_cities[index];
+}
+
+const std::vector<City> & TSPGraph::getCities() const {
+  return m_cities;
+}
+
+const double * TSPGraph::operator [] (int i) const {
+  return &m_distanceMatrix[i * m_numCities];
+}
+
+double * TSPGraph::operator [] (int i) {
+  return &m_distanceMatrix[i * m_numCities];
+}
---
+++ 02/TSPGraph.h
@@ -0,0 +1,33 @@
+#ifndef TSPGRAPH_H
+#define TSPGRAPH_H
+
+#include <vector>
+#include "City.h"
+
+class TSPGraph {
+public:
+  // Constructors
+  TSPGraph(int numCities, double width, double height);
+
+  // Getters
+  double getDistance(int from, int to) const;
+  int getNumCities() const;
+  const City & getCity(int index) const;
+  const std::vector<City> & getCities() const;
+
+  // Overloading [] so that we can index array as if it was matrix
+  // The first [] will return start of the row
+  // The second [] will then return the actual element
+  const double * operator [] (int i) const;
+  double * operator [] (int i);
+
+
+private:
+  int m_numCities;
+  std::vector<City> m_cities;
+  std::vector<double> m_distanceMatrix; // 1D Array but it's actually a Matrix
+
+  void precomputeDistanceMatrix();
+};
+
+#endif // TSPGRAPH_H
