16faa0e630c74f856685cc2e9d39e38aeba30b7e
Marek Sobotka <sobotma8@fit.cvut.cz>
2026-04-03 21:31:38 +0200

    main.jl upraven

--- semestralka/src/main.jl
+++ semestralka/src/main.jl
@@ -1,14 +1,30 @@
-#!/usr/bin/env julia
 include("Generator.jl")
 include("Solver.jl")
+include("Visualizer.jl")
 
 using .SudokuGenerator
 using .SudokuSolver
+using .SudokuVisualizer
 
-board = generate_puzzle(board_size=25, cells_empty=450)
-println("9x9 Generated Sudoku Board")
+dim = 25
+println("Generating ", dim, "x", dim, " puzzle...")
+@time board = SudokuBoard(generate_puzzle_ip(25, 400))
 print_board(board)
 
-println("\nSolved by Integer Programming")
-solved = sudoku_solver_ip(board)
-print_board(solved)
+# println("Opening visualizer for Unsolved Board...")
+# draw_board(board, "Unsolved 9x9 Sudoku")
+
+# Pause so you can look at the unsolved board before it solves it
+# println("Press Enter to solve...")
+# readline()
+
+println("Solving with Integer Programming...")
+@time solved_grid = sudoku_solver_ip(board)
+print_board(solved_grid)
+
+# println("Opening visualizer for Solved Board...")
+# draw_board(solved_grid, board, "Solved by IP (100% Guarantee)")
+
+# Keep the window open at the end until you hit Enter again
+# println("Press Enter to exit...")
+# readline()
