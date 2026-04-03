a09da3772a0c108fa592aa74dbff96ffa7494df5
Marek Sobotka <sobotma8@fit.cvut.cz>
2026-04-03 23:59:09 +0200

    Interactive window

--- semestralka/src/Visualizer.jl
+++ semestralka/src/Visualizer.jl
@@ -2,7 +2,7 @@ module SudokuVisualizer
 
 using GLMakie
 
-export draw_board
+export draw_board, draw_interactive_board
 
 # Unsolved
 function draw_board(board, title_text::String="Sudoku Board")
@@ -74,4 +74,89 @@ function _draw_core(grid::Matrix{Int}, original_grid::Matrix{Int}, title_text::S
   display(fig)
   return fig
 end # function _draw_core
+
+function draw_interactive_board(board, solver_func)
+  grid_orig = typeof(board) <: Matrix ? board : board.grid
+  s = size(grid_orig, 1)
+  b = round(Int, sqrt(s))
+
+  fig = Figure(size=(1200, 1300))
+
+  # Sudoku Board
+  ax = Axis(fig[1, 1], title="Sudoku (Press ENTER to solve)", titlesize=30, aspect=DataAspect())
+  hidedecorations!(ax)
+  hidespines!(ax)
+
+  function draw_grid!(target_ax, current_grid)
+    draw_grid!(target_ax, current_grid.grid)
+  end
+  function draw_grid!(target_ax, current_grid::Matrix{Int})
+    empty!(target_ax) # Clears the board
+
+    # Lines
+    for i in 0:s
+      linewidth = (i % b == 0) ? 4.0 : 1.0
+      color     = (i % b == 0) ? :black : :gray
+      lines!(target_ax, [i, i], [0, s], color=color, linewidth=linewidth)
+      lines!(target_ax, [0, s], [i, i], color=color, linewidth=linewidth)
+    end
+
+    # Numbers
+    for r in 1:s
+      for c in 1:s
+        val = current_grid[r, c]
+        if val != 0
+          x_pos = c - 0.5
+          y_pos = (s - r) + 0.5
+          f_size = 96 / (ndigits(s) + 1)
+          text_color = (grid_orig[r, c] == 0) ? :blue : :black
+
+          text!(
+              target_ax, string(val),
+              position=(x_pos, y_pos),
+              align=(:center, :center),
+              fontsize=f_size,
+              color=text_color
+          )
+        end
+      end
+    end
+  end # function draw_grid!
+
+  # Unsolved
+  draw_grid!(ax, grid_orig)
+
+  is_solved = false
+
+  # Wait for action
+  function trigger_solve()
+    if !is_solved
+      # Solver from main
+      time_taken = @elapsed begin
+        solved_board = solver_func(board)
+      end
+      formatted_time = round(time_taken, digits=3)
+
+      if solved_board !== nothing
+        grid_solved = typeof(solved_board) <: Matrix ? solved_board : solved_board.grid
+        draw_grid!(ax, grid_solved)
+
+        ax.title = "Sudoku - Solved in $formatted_time seconds!"
+      else
+        ax.title = "Sudoku - No Solution Found! (Failed after $formatted_time seconds)"
+      end
+      is_solved = true
+    end
+  end
+
+  # Keyboard enter
+  on(events(fig).keyboardbutton) do event
+    if event.action == Keyboard.press && event.key == Keyboard.enter
+      trigger_solve()
+    end
+  end
+
+  display(fig)
+  return fig
+end # function draw_interactive_board
 end # module SudokuVisualizer
--- semestralka/src/main.jl
+++ semestralka/src/main.jl
@@ -6,25 +6,11 @@ using .SudokuGenerator
 using .SudokuSolver
 using .SudokuVisualizer
 
-dim = 25
+
+dim = 16
 println("Generating ", dim, "x", dim, " puzzle...")
-@time board = SudokuBoard(generate_puzzle_ip(25, 400))
-print_board(board)
+board = SudokuBoard(generate_puzzle_ip(dim, round(Int, dim^2 * 0.65)))
+draw_interactive_board(board, sudoku_solver_ip)
 
-# println("Opening visualizer for Unsolved Board...")
-# draw_board(board, "Unsolved 9x9 Sudoku")
-
-# Pause so you can look at the unsolved board before it solves it
-# println("Press Enter to solve...")
-# readline()
-
-println("Solving with Integer Programming...")
-@time solved_grid = sudoku_solver_ip(board)
-print_board(solved_grid)
-
-# println("Opening visualizer for Solved Board...")
-# draw_board(solved_grid, board, "Solved by IP (100% Guarantee)")
-
-# Keep the window open at the end until you hit Enter again
-# println("Press Enter to exit...")
-# readline()
+println("Press ENTER to end program")
+readline()
