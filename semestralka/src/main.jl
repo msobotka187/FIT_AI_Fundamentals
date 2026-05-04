014d280095916ccb14df292772e573585b737cea
Marek Sobotka <sobotma8@fit.cvut.cz>
2026-05-04 10:10:07 +0200

    9x9 default

--- semestralka/src/main.jl
+++ semestralka/src/main.jl
@@ -6,8 +6,7 @@ using .SudokuGenerator
 using .SudokuSolver
 using .SudokuVisualizer
 
-
-dim = 16
+dim = 9
 println("Generating ", dim, "x", dim, " puzzle...")
 board = SudokuBoard(generate_puzzle_ip(dim, round(Int, dim^2 * 0.65)))
 draw_interactive_board(board, sudoku_solver_ip)
