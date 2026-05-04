32b6f5af37f0f5cbb5f579c2f0e8bc292c70e692
Marek Sobotka <sobotma8@fit.cvut.cz>
2026-05-04 10:19:01 +0200

    Added Revise

--- semestralka/src/main.jl
+++ semestralka/src/main.jl
@@ -6,10 +6,11 @@ using .SudokuGenerator
 using .SudokuSolver
 using .SudokuVisualizer
 
-dim = 9
-println("Generating ", dim, "x", dim, " puzzle...")
-board = SudokuBoard(generate_puzzle_ip(dim, round(Int, dim^2 * 0.65)))
-draw_interactive_board(board, sudoku_solver_ip)
+function run(dim::Int=9)
+  println("Generating ", dim, "x", dim, " puzzle...")
+  board = SudokuBoard(generate_puzzle_ip(dim, round(Int, dim^2 * 0.65)))
+  draw_interactive_board(board, sudoku_solver_ip)
 
-println("Press ENTER to end program")
-readline()
+  println("Press ENTER to end program")
+  readline()
+end
